a1d658b53fc847c767c278392a6e3dc6c7e3ae9d
suefengcheng@gmail.com <suefengcheng@gmail.com>
2015-08-24 17:21:22 +0800

    基本畫面

---
+++ LandAssetsWebProject/views/scripts/changePassword.js
@@ -0,0 +1,58 @@
+﻿    function showChangePasswordWindow(userName) {
+
+        var wnd = $("#workspace_changePassword").data("kendoWindow"),
+            detailsTemplate;
+        
+        if (wnd == undefined)
+            wnd = $("#workspace_changePassword")
+                            .kendoWindow({
+                                title: "使用者名稱與密碼",
+                                modal: true,
+                                visible: false,
+                                resizable: false,
+                                width: 300
+                            }).data("kendoWindow");
+
+        detailsTemplate = kendo.template($("#changePassword-template").html());
+
+        wnd.content(detailsTemplate({ "userName": userName}));
+        wnd.center().open();
+    }
+
+    function closeChangePasswordWindow() {
+        $("#workspace_changePassword").data("kendoWindow").close();
+    }
+
+    function changePassword() {
+        var password = $("#txtPassword").val();
+        var userName = $("#txtUserName").val();
+
+        var data = {
+            "userName": userName,
+            "password": password
+        };
+
+        var menus_apiUrl = "Membership/Users";
+        $.ajax({
+            type: "PUT",
+            url: menus_apiUrl + "/" + userName,
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Basic ' + Cookies.get('token'));
+            },
+            success: function (data, status, jqXHR) {
+                debugger;
+                if (jqXHR.status==304) {
+                    //alert(jqXHR.responseText);
+                    alert("取消變更密碼!");
+                }
+                closeChangePasswordWindow();
+            },
+            error: function (xhr) {
+                alert(xhr.responseText);
+            }
+        });
+
+    }
---
+++ LandAssetsWebProject/views/scripts/mp_utilities.js
@@ -0,0 +1,48 @@
+﻿function getQueryStringByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function getObjectIndexOfArray(key, value, items) {
+    var return_value = -1;
+    for (var i = 0; i < items.length; i++) {
+        if ((key in items[i]) && items[i][key] == value) {
+            return_value= i;
+            break;
+        }
+    }
+    return return_value;
+}
+
+//http://www.telerik.com/forums/cannot-add-a-custom-http-header
+(function ($, kendo) {
+    "use strict";
+    kendo.MPAuthorization = kendo.MP || {};
+    kendo.MPAuthorization.DataSource = kendo.data.DataSource.extend({
+        init: function (options) {
+            if (options.transport && options.transport.read) {
+                options.transport.read.beforeSend = function (xhr) {
+                    xhr.setRequestHeader('Authorization', 'Basic ' + Cookies.get('token'));
+                };
+            }
+            if (options.transport && options.transport.update) {
+                options.transport.update.beforeSend = function (xhr) {
+                    xhr.setRequestHeader('Authorization', 'Basic ' + Cookies.get('token'));
+                };
+            }
+            if (options.transport && options.transport.destroy) {
+                options.transport.destroy.beforeSend = function (xhr) {
+                    xhr.setRequestHeader('Authorization', 'Basic ' + Cookies.get('token'));
+                };
+            }
+            if (options.transport && options.transport.create) {
+                options.transport.create.beforeSend = function (xhr) {
+                    xhr.setRequestHeader('Authorization', 'Basic ' + Cookies.get('token'));
+                };
+            }
+            kendo.data.DataSource.fn.init.call(this, options);
+        }
+    });
+})($, kendo);
